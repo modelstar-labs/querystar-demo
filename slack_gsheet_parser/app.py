1a755c992ef0fad1e39257563e325ec1206316fa
helix <helincao@gmail.com>
2023-09-17 20:24:39 -0700

    minor tweaks

--- slack_gsheet_parser/app.py
+++ slack_gsheet_parser/app.py
@@ -1,8 +1,9 @@
 import querystar as qs
 import openai
 import json
+import os
 
-openai.api_key = 'sk-***' #add your own openai api key
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def extract_product(message: str) -> dict:
     prompt = [
@@ -13,7 +14,7 @@ def extract_product(message: str) -> dict:
                         "in the user message, and what their category is. "
                         "Output a dict: \n"
                         "{\"product\": [\"product1\", \"product2\"],"
-                        "  \"category\": \"cloud service\"}\n\n"
+                        "  \"category\": \"product category\"}\n\n"
                         "If no product can be found, return an empty dict: {}")
         },
         {
@@ -27,26 +28,22 @@ def extract_product(message: str) -> dict:
         messages=prompt,
         temperature=0,
         max_tokens=500)
-    
     try:
         product = json.loads(response.choices[0].message['content'])
         product['valid GPT answer'] = True
     except:
         product = {'valid GPT answer': False}
-
+    
     return product
 
 
-message = qs.triggers.slack.new_message(
-    channel_id='C***',                  # add your own slack channel id
-    trigger_for_bot_messages=False
-    )
+message = qs.triggers.slack.new_message(channel_id='C05S0STJ1E1')   # add your own slack channel id
 
 product = extract_product(message['text'])
 
 qs.actions.google_sheets.add_row(
-    spreadsheet_id='1nCRGIMe10zmBpP2wQUHMFRvyuHfvoRTBJjw1CKVdDck',
-    worksheet_id='Sheet2',
+    spreadsheet_id='1nCRGIMe10zmBpP2wQUHMFRvyuHfvoRTBJjw1CKVdDck', # add your own Google Sheet id
+    worksheet_id='Sheet1',   # add your own Worksheet id
     data=[[message['user'], message['text'], product['valid GPT answer'], 
            repr(product['product']), product['category'] ]]
     )
